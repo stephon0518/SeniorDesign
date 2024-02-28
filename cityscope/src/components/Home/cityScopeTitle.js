86f79abe432d3aece8a5d41be58cce530d60b424
alsworth <84337775+alsworth@users.noreply.github.com>
2024-02-27 19:50:47 -0500

    Added Cityscope title / background

---
+++ cityscope/src/components/Home/cityScopeTitle.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./cityScopeTitle.css"
+import Typography from '@mui/material/Typography';
+
+
+export default function HomePage() {
+    return (
+
+        <div className="pageContainer">
+            <div className="pageTitle">
+                <Typography variant="h2" align="center">
+                    CityScope
+                </Typography>
+            </div>
+        </div>
+
+      );
+    }
+
--- cityscope/src/index.js
+++ cityscope/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import HomePage from './components/Home/cityScopeTitle';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <HomePage></HomePage>
   </React.StrictMode>
 );
 
