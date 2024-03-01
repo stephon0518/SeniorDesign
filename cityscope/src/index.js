bdaa330f6126b37ca66cc66e2cf4bd23d5febef1
alsworth <84337775+alsworth@users.noreply.github.com>
2024-03-01 18:35:08 -0500

    added search box / button

--- cityscope/src/index.js
+++ cityscope/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import HomePage from './components/Home/cityScopeTitle';
+import CityScopeHome from './components/cityScopeTitle/cityScopeHome';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <HomePage></HomePage>
+    <CityScopeHome/>
   </React.StrictMode>
 );
 
