85cf5f782df2aa70798cdb799fd700251193ffd7
MeadowsT <tamera.meadows@gmail.com>
2024-03-23 08:21:13 -0400

    Update cityScopeSearchResults.js

--- cityscope/src/components/cityScopeSearchResults/cityScopeSearchResults.js
+++ cityscope/src/components/cityScopeSearchResults/cityScopeSearchResults.js
@@ -6,11 +6,37 @@ import "./cityScopeSearchResults.css"
 export default function CityScopeSearchResults() {
     const location = useLocation();
     console.log(location)
-    
+    //rating
+    const [minRating, setMinRating] = useState(0);
+    const [priceFilter, setPriceFilter] = useState(null);
+    const filteredBusinesses = location.state.businesses.filter(
+        business => business.rating >= minRating&& (priceFilter ? business.price === priceFilter : true));
+        
+
 
     return (
         <div className="container">
-            <div>Search Results</div>
+            <div>
+                <h2>Search Results</h2>
+                <div>
+                    {/* Dropdown for rates */}
+                    <select value={ratingFilter} onChange={e => setRatingFilter(e.target.value || null)}>
+                        <option value="">Select Rating</option>
+                        {[1, 2, 3, 4, 5].map(rating => (
+                            <option key={rating} value={rating}>{rating}</option>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    {/* Dropdown for prices */}
+                    <select value={priceFilter} onChange={e => setPriceFilter(e.target.value || null)}>
+                        <option value="">Select Price</option>
+                        <option value="$">Low ($)</option>
+                        <option value="$$">Medium ($$)</option>
+                        <option value="$$$">High ($$$)</option>
+                    </select>
+                </div>
+            </div>
             {location.state.businesses.map((business, index) => (
                 <div key={index}>
                     <p>{business.name}</p>
